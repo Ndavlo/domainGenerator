a5f9a58ec6ac6d3cfe6a2947ccc061d121e8c2b3
Ndavlo <nicolasdl99@gmail.com>
2023-01-05 00:50:08 +0000

    Domain

--- src/app.js
+++ src/app.js
@@ -6,6 +6,33 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".com", ".us", ".mx", ".gob"];
+
+  function generateRandom() {
+    const ul = document.createElement("ul");
+    ul.classList.add("my-list");
+
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+          for (let l = 0; l < domain.length; l++) {
+            const li = document.createElement("li");
+            li.textContent = pronoun[i] + adj[j] + noun[k] + domain[l];
+            ul.appendChild(li);
+          }
+        }
+      }
+    }
+
+    const ulDiv = document.createElement("div");
+    ulDiv.classList.add("ulDiv");
+    ulDiv.appendChild(ul);
+    document.body.appendChild(ulDiv);
+  }
+
+  const button = document.querySelector("button");
+  button.addEventListener("click", generateRandom);
 };
